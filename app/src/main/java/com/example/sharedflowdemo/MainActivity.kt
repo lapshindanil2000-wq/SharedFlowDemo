b992ab6468db4c9217850cfa790111d93778ed63
ComposeDemo <danil.lapshin.2000@mail.ru>
2026-04-17 19:17:37 +0700

    test

--- app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
@@ -6,11 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.flow.SharedFlow
 import com.example.sharedflowdemo.ui.theme.SharedFlowDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,12 +41,30 @@ fun ScreenSetup(
     modifier: Modifier = Modifier,
     viewModel: DemoViewModel = viewModel()
 ) {
-    MainScreen(modifier)
+    MainScreen(modifier, viewModel.sharedFlow)
 }
+
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Main Screen",
-        modifier = modifier
-    )
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    sharedFlow: SharedFlow<Int>
+) {
+    val messages = remember { mutableStateListOf<Int>() }
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(key1 = Unit) {
+        sharedFlow.collect { value ->
+            messages.add(value)
+        }
+    }
+
+    LazyColumn(modifier = modifier) {
+        items(messages) { message ->
+            Text(
+                text = "Collected Value = $message",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(5.dp)
+            )
+        }
+    }
 }
